d1c4f37912b0eecaba0f4cf89a9693b7dfc44596
Kaviya <kaviya123@gmail.com>
2026-03-11 21:56:57 +0530

    Initial commit

---
+++ variables.py
@@ -0,0 +1,42 @@
+# Program 1
+name = "Darling"
+print(name)
+
+# Program 2
+age = 21
+print(age)
+
+# Program 3
+city = "Chennai"
+print(city)
+
+# Program 4
+a = 10
+b = 20
+print(a + b)
+
+# Program 5
+x = 5
+print(x * 2)
+
+# Program 6
+name = input("Enter your name: ")
+print("Welcome", name)
+
+# Program 7
+age = input("Enter your age: ")
+print("Age is", age)
+
+# Program 8
+a = 8
+b = 2
+print(a * b)
+
+# Program 9
+price = 100
+print("Price:", price)
+
+# Program 10
+x = 20
+y = 10
+print(x - y)
